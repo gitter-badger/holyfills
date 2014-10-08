8ba5a45780db50f0b2e0161964033c793bc6dfcb
tomchentw <developer@tomchentw.com>
2014-10-09 01:47:54 +0800

    chore(*): init project

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var webpack = require("webpack");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      HAS_ARRAY_PROTOTYPE_FOREACH: false
+    })
+    // new webpack.optimize.UglifyJsPlugin()
+  ]
+};
